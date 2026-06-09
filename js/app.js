8384c8eee7dc0cd287ac95673f9f9dc2628782bd
Jules <beugueum@gmail.com>
2026-06-09 09:40:47 +0100

    🎫 Billet luxueux avec photo mariés

--- js/app.js
+++ js/app.js
@@ -448,6 +448,8 @@ function roundRect(doc, x, y, w, h, r = 0, style = "S") {
  */
 
 async function genPDF(id) {
+    console.log("NOUVEAU PDF CHARGE");
+
   const g = guests.find(x => x.id === id);
   if (!g) return;
   notify("Génération du billet...", "inf");
